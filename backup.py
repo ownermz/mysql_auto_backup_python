67481be1627d8bba9e2ec922b40087b500178213
Arthur Floriani Martins <a.florianimartins@accenture.com>
2016-01-20 15:58:25 +0000

    Selecting last modified file to upload to dropbox. Added more comments

--- backup.py
+++ backup.py
@@ -6,7 +6,7 @@ import os
 import time
 import zlib
 import zipfile
-
+import sys
 
 
 import datetime
@@ -20,8 +20,10 @@ def init ():
     tries = 3
     backupDatabase()
     while tries > 0:
-        checkDropbox = uploadDropbox()
         checkBrck = uploadBrck()
+        checkBrck = True;
+        checkDropbox = uploadDropbox()
+        
 
         if checkBrck and checkDropbox:
             break
@@ -42,10 +44,13 @@ def init ():
 
 def cleanBackups():
     print "Checking files to be cleaned"
+
+    #Store in var lines all the files current on the BRCK
     with open('uploaded_to_brck.txt') as logFile:
         lines = logFile.read().splitlines()
 
     try:
+        #Create SSH and SCP session.
         ssh = createSSHClient()
         scp = SCPClient(ssh.get_transport())
 
@@ -57,20 +62,23 @@ def cleanBackups():
               curpath = os.path.join(dirpath, file)
               file_modified = datetime.datetime.fromtimestamp(os.path.getmtime(curpath))
               if len(filenames) > 30 and datetime.datetime.now() - file_modified > datetime.timedelta(days=30):
+                  #Check if file is in the BRCK
                   if 'backups/'+file in lines:
                       print 'Removing '+file+' from the BRCK'
                       ssh.exec_command('rm /home/openmrs/backups/'+file)
-
                       f = open("uploaded_to_brck.txt","w")
                       for line in lines:
                           if line!="backups/"+file:
                             f.write(line +"\n")
                       f.close()
+
+                  #Remove file from System
                   os.remove(curpath)
 
         ssh.close()
     except:
         print 'Failed connecting to the BRCK'
+        print "Error description:", sys.exc_info()[0]
 
 
     return
@@ -102,31 +110,45 @@ def backupDatabase ():
 def uploadDropbox ():
 
     if internet_on():
-        # Get your app key and secret from the Dropbox developer website
+
 
         try:
             print 'Connecting to Dropbox'
-            client = dropbox.client.DropboxClient('YF9nIzoNBSAAAAAAAAAABvso-pNu0CgnHnbe-lTNVEu-m0d0nShJJh5Mq8LrRz7A')
+            #This token can be generated on the dropbox developer page https://www.dropbox.com/developers/apps
+            client = dropbox.client.DropboxClient('YF9nIzoNBSAAAAAAAAAAKp_Ap5coyv6mOduPRoA3-UTFrOoS98B3Lwo9XvRYVBZm')
 
+            #Open on var lines all the files that were uploaded to dropbox
             with open('uploaded_to_dropbox.txt') as logFile:
                 lines = logFile.read().splitlines()
 
-            # Writing log file
+            # Opening log file
             log = open('uploaded_to_dropbox.txt', 'a')
-            #Checks for files to be uploaded
-            for file in glob.glob("backups/*.zip"):
-                if file not in lines:
-                    print 'Uploading ' + file
-                    f = open(file, 'rb')
-                    fileName = file.replace("\\", "/")
-                    #Uploads to dropbox
-                    response = client.put_file(fileName, f)
-                    #If file is uploaded, add filename to log
-                    if response:
-                        print 'Uploaded with Success'
-                        log.write( file +'\n' )
+
+
+            #Script from stack_overflow for searching for the last modified file. This will select which file should be sent to dropbox
+            max_mtime = 0
+            for dirname,subdirs,files in os.walk("backups"):
+                for fname in files:
+                    full_path = os.path.join(dirname, fname)
+                    mtime = os.stat(full_path).st_mtime
+                    if mtime > max_mtime:
+                        max_mtime = mtime
+                        max_dir = dirname
+                        max_file = fname
+
+
+            file = max_dir + "/" +max_file
+            print 'Uploading ' + file
+            f = open(file, 'rb')
+            #Uploads to dropbox
+            response = client.put_file(file, f)
+            #If file is uploaded, add filename to log
+            if response:
+                print 'Uploaded with Success'
+                log.write( file +'\n' )
         except:
-            print "Error uploading to dropbox"
+            print "Error uploading to dropbox. Check Dropbox Auth Token and Internet connection"
+            print "Error description:", sys.exc_info()[0]
             return False
     else:
         print 'No internet connection'
@@ -153,12 +175,16 @@ def uploadBrck ():
         ssh = createSSHClient()
         scp = SCPClient(ssh.get_transport())
 
+
+        #Open in the var lines all the files names that are currently on the BRCK.
         with open('uploaded_to_brck.txt') as logFile:
             lines = logFile.read().splitlines()
 
+        #Open log for appending
         log = open('uploaded_to_brck.txt', 'a')
         #Checks for files to be uploaded
         for file in glob.glob("backups/*.zip"):
+            #If file was not sent to the BRCK
             if file not in lines:
                 print 'Uploading ' + file
                 f = open(file, 'rb')
@@ -170,9 +196,8 @@ def uploadBrck ():
         return True
     except:
         print "Failed connecting to the BRCK. Check if PC is connected to the BRCK WiFi"
+        print "Error description:", sys.exc_info()[0]
         return False
-
-    return False
 
 
 def internet_on():
